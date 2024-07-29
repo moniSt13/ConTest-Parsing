6dde2f1a62336ee681b63ce5da69a4b8ad22fe43
c7031356 <monika.steidl@uibk.ac.at>
2024-07-29 14:00:24 +0200

    version updates in setup.py because functions not available anymore

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='jaeger_prometheus_joining',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.2',
     description='Join Jaeger-Traces and Prometheus-Metrics',
     author='Me',
     license='MIT',
-    install_requires=['pandas', 'polars', 'pyarrow', 'neo4j', 'neo4jvis', 'ipython', 'logparser3']
+    install_requires=['pandas==2.2.2', 'polars==0.20.23', 'pyarrow==17.0.0', 'neo4j==5.20.0', 'neo4jvis==0.0.1', 'ipython==8.26.0', 'logparser3']#, 'neomodel==5.3.0']
 )
